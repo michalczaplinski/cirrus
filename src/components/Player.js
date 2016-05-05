79ebe88458fa839bbae84100e300b7b7fa637d97
Michal Czaplinski <mmczaplinski@gmail.com>
2016-05-05 18:45:57 +0200

    added the images for logos, SC and connect buttons
    the app now:
      - retrieves the initial data.
      - retrieves the user data

---
+++ src/components/Player.js
@@ -0,0 +1,12 @@
+import React, {PropTypes} from 'react';
+
+const Player = ({  }) => {
+
+  return (
+    <div></div>
+  );
+};
+
+export default Player;
+
+
--- src/reducers/initialState.js
+++ src/reducers/initialState.js
@@ -1,11 +1,8 @@
 var initialState = {
   ready: true,
   client_id: 'ac7b31ea50f2019f548ffb8f7056b41d',
-  redirect_uri: 'http://localhost:4321/callback.html',
-  initial_data: {
-    is_loading: false,
-    tracks: []
-  }
+  redirect_uri: 'http://localhost:3006/callback.html',
+  initial_data: []
 };
 
 export default initialState;
